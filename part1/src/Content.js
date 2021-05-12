aaa0cafc8d6acc232fe6759fa1d9d0f7bae9bc82
devcraft1 <ukonudennis64@gmail.com>
2021-05-12 22:21:20 +0100

    part1

--- part1/src/Content.js
+++ part1/src/Content.js
@@ -1,24 +1,47 @@
 import React from "react";
 
-const Content = () => {
+const Part1 = () => {
   const part1 = "Fundamentals of React";
   const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
-
   return (
     <div>
       <p>
         {part1} {exercises1}
       </p>
+    </div>
+  );
+};
+
+const Part2 = () => {
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  return (
+    <div>
       <p>
         {part2} {exercises2}
       </p>
+    </div>
+  );
+};
+
+const Part3 = () => {
+  const part3 = "State of a component";
+  const exercises3 = 14;
+  return (
+    <div>
       <p>
         {part3} {exercises3}
       </p>
+    </div>
+  );
+};
+
+const Content = () => {
+  return (
+    <div>
+      <Part1 />
+      <Part2 />
+      <Part3 />
     </div>
   );
 };
