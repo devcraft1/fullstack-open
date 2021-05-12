24765630db3d3a274d2d28b58ddc48ca2b8a54d6
devcraft1 <ukonudennis64@gmail.com>
2021-05-12 22:33:00 +0100

    part1

--- part1/src/Content.js
+++ part1/src/Content.js
@@ -1,47 +1,47 @@
 import React from "react";
 
-const Part1 = () => {
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
+const Part1 = (props) => {
   return (
     <div>
       <p>
-        {part1} {exercises1}
+        {props.part1} {props.exercises1}
       </p>
     </div>
   );
 };
 
-const Part2 = () => {
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
+const Part2 = (props) => {
   return (
     <div>
       <p>
-        {part2} {exercises2}
+        {props.part2} {props.exercises2}
       </p>
     </div>
   );
 };
 
-const Part3 = () => {
-  const part3 = "State of a component";
-  const exercises3 = 14;
+const Part3 = (props) => {
   return (
     <div>
       <p>
-        {part3} {exercises3}
+        {props.part3} {props.exercises3}
       </p>
     </div>
   );
 };
 
 const Content = () => {
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
   return (
     <div>
-      <Part1 />
-      <Part2 />
-      <Part3 />
+      <Part1 part1={part1} exercises1={exercises1} />
+      <Part2 part2={part2} exercises2={exercises2} />
+      <Part3 part3={part3} exercises3={exercises3} />
     </div>
   );
 };
