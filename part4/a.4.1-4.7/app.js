0cb3e30fcb0e7afbdeef04df76d191e64b44b9bc
devcraft1 <ukonudennis64@gmail.com>
2021-08-19 18:32:40 +0100

    blog

---
+++ part4/a.4.1-4.7/app.js
@@ -0,0 +1,40 @@
+const config = require("./utils/config");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const mongoose = require("mongoose");
+const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
+const blogRouter = require("./controllers/blog");
+
+logger.info("connecting to", config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    logger.info("connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("error connecting to MongoDB:", error.message);
+  });
+
+app.get("/", (request, response) => {
+  response.send("welcome to the home page");
+});
+
+app.use(cors());
+app.use(express.static("build"));
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+app.use("/api/blogs", blogRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
---
+++ part4/a.4.1-4.7/controllers/blog.js
@@ -0,0 +1,26 @@
+const blogRouter = require("express").Router();
+const Blog = require("../model/blog");
+const logger = require("../utils/logger");
+
+blogRouter.get("/", (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+  // .catch((error) => next(error));
+});
+
+blogRouter.post("/", (request, response) => {
+  const body = request.body;
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  });
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+  // .catch((error) => next(error));
+});
+
+module.exports = blogRouter;
