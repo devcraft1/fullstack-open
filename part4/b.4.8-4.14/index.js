1e16a6c1863321c8059ae3e7a8332e7af5195229
devcraft1 <ukonudennis64@gmail.com>
2021-08-23 22:55:33 +0100

    test

--- part4/b.4.8-4.14/index.js
+++ part4/b.4.8-4.14/index.js
@@ -1,10 +1,12 @@
 const http = require("http");
 const app = require("./app");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
 
 const server = http.createServer(app);
 
-const port = process.env.PORT || 3003;
+// const port = process.env.PORT || 3003;
 
-server.listen(port, () => {
-  console.log(`server started on http://localhost:${port}`);
+server.listen(config.PORT, () => {
+  logger.info(`server started on http://localhost:${config.PORT}`);
 });
